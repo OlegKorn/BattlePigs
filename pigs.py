7e5860df8f1fe750547ceb848d5800859195ad17
O <48214249+OlegKorn@users.noreply.github.com>
2021-08-29 14:53:50 +0300

    Update pigs.py
            resized_flag = im_to_paste.resize((110, 55))
    
            if (w != 0 and h):
                resized_flag = im_to_paste.resize((int(w), int(h*0.3)))
    
            if (w == 0 and h):
                resized_flag = im_to_paste.resize((0, int(h * 0.3)))

--- pigs.py
+++ pigs.py
@@ -1,6 +1,7 @@
 import sys
 from PIL import Image, ImageDraw
 import os
+import random
 
 
 class Pig:
@@ -108,15 +109,28 @@ class Pig:
         draw_obj.rectangle(stripe_2, self.YELLOW)
 
 
-    def insert_flag(self, im, path):
-        im_to_paste = Image.open(f'{self.path}/_flag_bytan_new.jpg')
-        resized_flag = im_to_paste.resize((100, 60))
+    def insert_flag(self, im=None, path=None, w=None, h=None):
+        # flag files
+        flags = os.listdir(f'{self.path}/banners')
+        flag = random.choice(flags)
+        print(flag)
+        
+        im_to_paste = Image.open(f'{self.path}/banners/{flag}')
+
+        resized_flag = im_to_paste.resize((110, 55))
+
+        if (w != 0 and h):
+            resized_flag = im_to_paste.resize((int(w), int(h*0.3)))
+
+        if (w == 0 and h):
+            resized_flag = im_to_paste.resize((0, int(h * 0.3)))    
+        
         im.paste(resized_flag)
         im.save(f'{self.path}/FLAG.jpg')
-
+        
 
     def main(self):
-        for pic in os.listdir(self.path)[0:2]:
+        for pic in os.listdir(self.path)[0:10]:
             number_of_files = len(os.listdir(self.path))
 
             if (not pic.endswith('.py')) and (not pic.endswith('.log')):
@@ -127,18 +141,20 @@ class Pig:
                     print()
                     print(im.width, im.height)
 
+                    '''
                     self.draw_quotation_mark(draw, im.width, im.height)
                     self.draw_ukrainian_banner(draw, im.width, im.height)
                     im.save(f'UKR_{title}.jpeg')                    
                     print(f'Created: UKR_{title}.jpeg')
                     
+                    '''
                     self.draw_quotation_mark(draw, im.width, im.height)
                     self.draw_russian_banner(draw, im.width, im.height)
                     im.save(f'RUS_{title}.jpeg')
                     print(f'Created: RUS_{title}.jpeg')
-
+                    
                     # insert flag
-                    self.insert_flag(im, self.path)
+                    self.insert_flag(im, self.path, im.height)
 
 
 
